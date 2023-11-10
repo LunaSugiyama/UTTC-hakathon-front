f215098fd04e3067e5123293ba28458722c3c2ca
LunaSugiyama <lunasugiyama13@gmail.com>
2023-11-10 11:18:01 +0900

    made searchItem

--- src/components/Item/SearchItem.tsx
+++ src/components/Item/SearchItem.tsx
@@ -1,137 +1,157 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Cookies from 'js-cookie';
 import { Link } from 'react-router-dom';
 import { Item } from '../../interfaces/Item';
+import CurriculumCheckbox from '../curriculum/Checkbox';
+import { set } from 'firebase/database';
+
+interface Curriculum {
+    id: number;
+    name: string;
+}
 
 const SearchItem: React.FC = () => {
-  const [searchResults, setSearchResults] = useState<Item[]>([]);
-  const [searchQuery, setSearchQuery] = useState<string>('');
-  const [currentInput, setCurrentInput] = useState<string>(''); // Current input in the search box
-  const [sortCriteria, setSortCriteria] = useState('created_at'); // Default sort criteria
-  const [sortOrder, setSortOrder] = useState('asc'); // Default sort order
+    const [searchResults, setSearchResults] = useState<Item[]>([]);
+    const [searchQuery, setSearchQuery] = useState<string>('');
+    const [sortedResults, setSortedResults] = useState<Item[]>([]);
+    const [sortCriteria, setSortCriteria] = useState<keyof Item>('CreatedAt'); // Default sort criteria
+    const [sortOrder, setSortOrder] = useState('asc'); // Default sort order
+    const [selectedCurriculumIds, setSelectedCurriculumIds] = useState<number[]>([]);
+    const [curriculumData, setCurriculumData] = useState<Curriculum[]>([]);
+    const token = Cookies.get('token');
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.post('http://localhost:8000/items/search', {
-        words: searchQuery,
-        item_categories: 'your item categories',
-        curriculum_ids: 'your curriculum IDs',
-      });
-      setSearchResults(response.data);
-    } catch (error) {
-      // Handle error
-      console.error('Error fetching data:', error);
-    }
-  };
+    useEffect(() => {
+        const fetchCurriculumData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8000/curriculums/showall', {
+                    headers: { Authorization: `Bearer ${token}` }
+                });
+                setCurriculumData(response.data);
+                setSelectedCurriculumIds(response.data.map((curriculum: Curriculum) => curriculum.id)); // Typed parameter (fix for error 1)
+            } catch (error) {
+                console.error('Error fetching curriculum data:', error);
+            }
+        };
 
-  useEffect(() => {
-    fetchData();
-  }, [searchQuery]); // Empty dependency array for first rendering  
+        fetchCurriculumData();
+    }, []);
 
-  const sortResults = () => {
-    const sortedResults = [...searchResults];
-    sortedResults.sort((a, b) => {
-      let comparison = 0;
-      if (sortOrder === 'asc') {
-        if (sortCriteria === 'created_at') {
-          comparison = a.CreatedAt.localeCompare(b.CreatedAt);
-        } else if (sortCriteria === 'updated_at') {
-          comparison = a.UpdatedAt.localeCompare(b.UpdatedAt);
-        } else if (sortCriteria === 'likes') {
-          comparison = a.Likes - b.Likes;
-        }
-      } else {
-        if (sortCriteria === 'created_at') {
-          comparison = b.CreatedAt.localeCompare(a.CreatedAt);
-        } else if (sortCriteria === 'updated_at') {
-          comparison = b.UpdatedAt.localeCompare(a.UpdatedAt);
-        } else if (sortCriteria === 'likes') {
-          comparison = b.Likes - a.Likes;
-        }
-      }
-      return comparison;
-    });
-  
-    setSearchResults(sortedResults);
-  };
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.post('http://localhost:8000/items/search', {
+              words: searchQuery,
+              curriculum_ids: selectedCurriculumIds.join(','),
+            });
+            // Ensure the response data is an array of Items
+            const items: Item[] = response.data;
+            // Add a unique id to each item using the index from the map function
+            const resultsWithUniqueId = items.map((item, index) => {
+              // Here, item is already typed as Item, and index is a number
+              return { ...item, uniqueId: `unique-${index}` }; // uniqueId is now a string to ensure uniqueness
+            });
+            setSearchResults(resultsWithUniqueId);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+        fetchData();
+      }, [searchQuery, selectedCurriculumIds]);      
 
-  const handleApplyFilter = () => {
-    sortResults(); // Apply the sort when "Apply Filter" button is clicked
-  };
+    const applyFilterAndSort = () => {
+        const filteredResults = searchResults.filter(item =>
+            selectedCurriculumIds.some(id => item.CurriculumIDs?.includes(id)) 
+            );
 
-  const handleEnterKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-        setSearchQuery(event.currentTarget.value);
-      sortResults(); // Apply the sort when Enter key is pressed
-    }
-  };
+        const sorted = filteredResults.sort((a, b) => {
+            const aValue = a[sortCriteria];
+            const bValue = b[sortCriteria];
+            if (typeof aValue === 'string' && typeof bValue === 'string') {
+                return sortOrder === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+            } else if (typeof aValue === 'number' && typeof bValue === 'number') {
+                return sortOrder === 'asc' ? aValue - bValue : bValue - aValue;
+            }
+            return 0;
+        });
+        setSortedResults(sorted);
+        console.log(sortedResults)
+    };
 
-  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSortCriteria(event.target.value as keyof Item);
-  };
+    useEffect(() => {
+        applyFilterAndSort();
+    }, [searchResults, sortCriteria, sortOrder, selectedCurriculumIds]);
 
-  const handleOrderChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSortOrder(event.target.value);
-  };
+    const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(event.target.value);
+    };
 
-  const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrentInput(event.target.value);
-  };
+    const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSortCriteria(event.target.value as keyof Item);
+    };
 
-  return (
-    <div>
-      <h1>Search Results</h1>
-      <div>
-        <label>Sort by:</label>
-        <select value={sortCriteria} onChange={handleSortChange}>
-          <option value="created_at">Created At</option>
-          <option value="updated_at">Updated At</option>
-          <option value="likes">Likes</option>
-        </select>
-        <select value={sortOrder} onChange={handleOrderChange}>
-          <option value="asc">Ascending</option>
-          <option value="desc">Descending</option>
-        </select>
-        <input
-          type="text"
-          placeholder="Enter search query"
-          value={currentInput}
-          onChange={handleSearchInputChange}
-          onKeyPress={handleEnterKeyPress}
-        />
-        <button onClick={handleApplyFilter}>Apply Filter</button>
-      </div>
-      <ul>
-        {searchResults.map((result) => (
-          <li key={result.Id}>
-            <Link
-              to={{
-                pathname: `/item/${result.ItemCategoriesName}/${result.Id}`,
-              }}
-              target="_blank"
-            >
-              <button>
-                User ID: {result.UserFirebaseUID} <br />
-                Title: {result.Title} <br />
-                Author: {result.Author} <br />
-                Link: {result.Link} <br />
-                Likes: {result.Likes} <br />
-                Item Category: {result.ItemCategoriesName} <br />
-                Created At: {result.CreatedAt} <br />
-                Updated At: {result.UpdatedAt} <br />
-                {result.CurriculumIds && result.CurriculumIds.length > 0 && (
-                  <>
-                    Curriculum IDs: {result.CurriculumIds.join(', ')}
-                    <br />
-                  </>
-                )}
-              </button>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    const handleOrderChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSortOrder(event.target.value);
+    };
+
+    const handleCheckboxChange = (id: number) => {
+        setSelectedCurriculumIds(prevSelected =>
+            prevSelected.includes(id) ? prevSelected.filter(curriculumId => curriculumId !== id) : [...prevSelected, id]
+        );
+    };
+
+    return (
+        <div>
+            <h1>Search Results</h1>
+            <div>
+                <label>Sort by:</label>
+                <select value={sortCriteria} onChange={handleSortChange}>
+                    <option value="CreatedAt">Created At</option>
+                    <option value="UpdatedAt">Updated At</option>
+                    <option value="Likes">Likes</option>
+                </select>
+                <select value={sortOrder} onChange={handleOrderChange}>
+                    <option value="asc">Ascending</option>
+                    <option value="desc">Descending</option>
+                </select>
+                <CurriculumCheckbox
+                    selectedCurriculumIds={selectedCurriculumIds}
+                    onCheckboxChange={handleCheckboxChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Enter search query"
+                    value={searchQuery}
+                    onChange={handleSearchInputChange}
+                />
+                <button onClick={() => applyFilterAndSort()}>Apply Filter</button>
+            </div>
+            <ul>
+                {sortedResults.map((result) => (
+                    <li key={result.UniqueId}>
+                        <Link to={`/item/${result.ItemCategoriesName}/${result.ID}`} target="_blank">
+                            <button>
+                                User ID: {result.UserFirebaseUID} <br />
+                                Title: {result.Title} <br />
+                                Author: {result.Author} <br />
+                                Link: {result.Link} <br />
+                                Likes: {result.Likes} <br />
+                                Item Category: {result.ItemCategoriesName} <br />
+                                Created At: {result.CreatedAt} <br />
+                                Updated At: {result.UpdatedAt} <br />
+                                {result.CurriculumIDs && result.CurriculumIDs.length > 0 && (
+                                    <>
+                                        Curriculum IDs: {result.CurriculumIDs.join(', ')}
+                                        <br />
+                                    </>
+                                )}
+                            </button>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default SearchItem;
